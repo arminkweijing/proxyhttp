6c6e39cd1ac82e74117d3c7206df19ee3dd7ac0b
zhangling <zhangling.rd@gmail.com>
2017-08-20 14:46:13 +0800

    fix

--- README.md
+++ README.md
@@ -15,17 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // 本地调用测试环境 api, 需要连接 vpn
-app.use("/api", require("proxyhttp")())
-```
-
-项目根目录配置 `proxy_conf.json`
-```js
-{
+app.use("/api", require("proxyhttp")({
     // 内网对应的公网IP(若不设置，默认外网服务)
     "ip": "112.64.124.86",
     // 内网服务地址
     "intranet": "http://192.168.4.224:3011",
     // 外网服务地址
     "extranet": "http://192.168.101.7:3011"
-}
+}))
 ```
--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 request = require('request');
-var config = require('./../../proxy_conf.json');
 
-// 接口转发
-function transfer(ip, req, res) {
-    // 根据外网 ip 切换调用内网、外网环境api
-    // 转发逻辑
-    agent = req.get('User-Agent');
-    options = {
-        'url': config[(ip == config.ip ? 'intranet' : 'extranet')] + req.baseUrl + req.path,
-        'method': req.method,
-        'qs': req.query,
-        'gzip': true,
-        'headers': {
-            'User-Agent': agent,
-            'X-Forwarded-For': req.ip
+
+module.exports = function(obj) {
+    var config = obj
+
+    // 接口转发
+    function transfer(ip, req, res) {
+        // 根据外网 ip 切换调用内网、外网环境api
+        // 转发逻辑
+        agent = req.get('User-Agent');
+        options = {
+            'url': config[(ip == config.ip ? 'intranet' : 'extranet')] + req.baseUrl + req.path,
+            'method': req.method,
+            'qs': req.query,
+            'gzip': true,
+            'headers': {
+                'User-Agent': agent,
+                'X-Forwarded-For': req.ip
+            }
         }
+
+        if (req.is('json')) {
+            options.json = true;
+            options.body = req.body;
+        } else if (req.is('urlencoded')) {
+            options.form = req.body
+        }
+        console.info(options.method, options.url)
+        console.dir(options.method == 'GET' ? req.query : req.body)
+        console.log('\n')
+
+        request(options).pipe(res);
     }
 
-    if (req.is('json')) {
-        options.json = true;
-        options.body = req.body;
-    } else if (req.is('urlencoded')) {
-        options.form = req.body
-    }
-    console.info(options.method, options.url)
-    console.dir(options.method == 'GET' ? req.query : req.body)
-    console.log('\n')
-
-    request(options).pipe(res);
-}
-
-module.exports = function() {
     return function proxy(req, res) {
         if (config.ip) require('./getIp')(transfer, req, res)
         else transfer('', req, res)
