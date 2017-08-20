7c39188cee2f628e40a358ecf9b61fadb046a378
zhangling <zhangling.rd@gmail.com>
2017-08-20 14:12:32 +0800

    fix

--- getIp.js
+++ getIp.js
@@ -3,22 +3,24 @@ var http = require('http')
 var cheerio = require('cheerio')
 var cache = require('memory-cache')
 
-module.exports = function(callback) {
+module.exports = function(callback, req, res) {
     var ip = cache.get('ip')
     if (ip) {
-        callback(ip)
+        callback(ip, req, res)
     } else {
         // 搜狐免费 ip 开放接口：http://pv.sohu.com/cityjson
-        http.get('http://pv.sohu.com/cityjson', function(res) {
+        http.get('http://pv.sohu.com/cityjson', function(response) {
             var body = ''
-            res.on('data', function(data) {
+            response.on('data', function(data) {
                 body += data
             })
-            res.on('end', function() {
+            response.on('end', function() {
                 eval(body)
                 ip = returnCitySN.cip
                 cache.put('ip', ip)
                 console.log(`>> 外网ip：${cache.get('ip')}      API：${ip == '112.64.124.86' ? '内网/开发环境' : '外网/测试环境'}`)
+
+                callback(ip, req, res)
             })
         }).on('error', function() {
             console.log('抓取 IP 出错')
--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 request = require('request');
+var config = require('./config.json');
+
+// 接口转发
+function transfer(ip, req, res) {
+    // 根据外网 ip 切换调用内网、外网环境api
+    // 转发逻辑
+    agent = req.get('User-Agent');
+    options = {
+        'url': config[(ip == config.ip ? 'intranet' : 'extranet')] + req.baseUrl + req.path,
+        'method': req.method,
+        'qs': req.query,
+        'gzip': true,
+        'headers': {
+            'User-Agent': agent,
+            'X-Forwarded-For': req.ip
+        }
+    }
+
+    if (req.is('json')) {
+        options.json = true;
+        options.body = req.body;
+    } else if (req.is('urlencoded')) {
+        options.form = req.body
+    }
+    console.info(options.method, options.url)
+    console.dir(options.method == 'GET' ? req.query : req.body)
+    console.log('\n')
+
+    request(options).pipe(res);
+}
 
 module.exports = function() {
     return function proxy(req, res) {
-        require('./getIp')(function(ip) {
-            // 根据外网 ip 切换调用内网、外网环境api
-            var config = require('./config.json');
-            // 转发逻辑
-            agent = req.get('User-Agent');
-            options = {
-                'url': config[(ip == config.ip ? 'intranet' : 'extranet')] + req.baseUrl + req.path,
-                'method': req.method,
-                'qs': req.query,
-                'gzip': true,
-                'headers': {
-                    'User-Agent': agent,
-                    'X-Forwarded-For': req.ip
-                }
-            }
-
-            if (req.is('json')) {
-                options.json = true;
-                options.body = req.body;
-            } else if (req.is('urlencoded')) {
-                options.form = req.body
-            }
-            console.info(options.method, options.url)
-            console.dir(options.method == 'GET' ? req.query : req.body)
-            console.log('\n')
-
-            request(options).pipe(res);
-        })
+        if (config.ip) require('./getIp')(transfer, req, res)
+        else transfer('', req, res)
     };
 };
